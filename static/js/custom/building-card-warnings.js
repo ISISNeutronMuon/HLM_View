5c3bff8126d66720d3f6c31ec8eb6051f339199e
Mihai Stavila <mihai.stavila@stfc.ac.uk>
2021-04-28 14:17:09 +0100

    Building cards & warnings small fix (document ready)

--- static/js/custom/building-card-warnings.js
+++ static/js/custom/building-card-warnings.js
@@ -2,79 +2,79 @@
 
 var buildingDataURL = JSON.parse(document.getElementById('general-data-url').textContent);
 
-function setWarnings() {
-    $.ajax({
-        url: buildingDataURL,
-        dataType: 'json',
-        success: function(data) {
-            // Stale devices
-            if (data.warnings.stale_devices.length > 0) {
-                // Display the stale devices warning icon
-                $('#stale-devices-wrapper').addClass('d-inline').removeClass('d-none');
-
-
-                const device_count = data.warnings.stale_devices.reduce((count, current) => count + current.length, 0);
-
-                // Append message with number of stale devices
-                $('#stale-devices-dd-menu').append(
-                    '<li>' +
-                    device_count +
-                    ' device(s) have stale values' +
-                    '</li>' +
-                    '<hr class="my-2">'
-                )
-
-                
-                // Append list of stale devices
-                data.warnings.stale_devices.forEach(coord_devices => {
-                    coord_devices.forEach(device => {
-                        $('#stale-devices-dd-menu').append(
-                            '<li class="font-weight-light">' +
-                            device +
-                            '</li>'
-                        )
-                    })
-                })
-            } else {
-                $('#stale-devices-wrapper').addClass('d-none').removeClass('d-inline');
-            }
-
-            // Devices with no Helium Litres value
-            if (data.warnings.no_value_devices.length > 0) {
-                // Display the warning icon
-                $('#no-value-devices-wrapper').addClass('d-inline').removeClass('d-none');
-
-                const device_count = data.warnings.no_value_devices.reduce((count, current) => count + current.length, 0);
-
-                // Append message with number of devices
-                $('#no-value-devices-dd-menu').append(
-                    '<li>' +
-                    device_count +
-                    ' device(s) have no He L value' +
-                    '</li>' +
-                    '<hr class="my-2">'
-                )
-
-                
-                // Append list of device names
-                data.warnings.no_value_devices.forEach(coord_devices => {
-                    coord_devices.forEach(device => {
-                        $('#no-value-devices-dd-menu').append(
-                            '<li class="font-weight-light">' +
-                            device +
-                            '</li>'
-                        )
-                    })
-                })
-            } else {
-                $('#no-value-devices-wrapper').addClass('d-none').removeClass('d-inline');
-            }
-
-        }
-    });
-}
-
 $(document).ready(function() {
+    function setWarnings() {
+        $.ajax({
+            url: buildingDataURL,
+            dataType: 'json',
+            success: function(data) {
+                // Stale devices
+                if (data.warnings.stale_devices.length > 0) {
+                    // Display the stale devices warning icon
+                    $('#stale-devices-wrapper').addClass('d-inline').removeClass('d-none');
+
+
+                    const device_count = data.warnings.stale_devices.reduce((count, current) => count + current.length, 0);
+
+                    // Append message with number of stale devices
+                    $('#stale-devices-dd-menu').append(
+                        '<li>' +
+                        device_count +
+                        ' device(s) have stale values' +
+                        '</li>' +
+                        '<hr class="my-2">'
+                    )
+
+                    
+                    // Append list of stale devices
+                    data.warnings.stale_devices.forEach(coord_devices => {
+                        coord_devices.forEach(device => {
+                            $('#stale-devices-dd-menu').append(
+                                '<li class="font-weight-light">' +
+                                device +
+                                '</li>'
+                            )
+                        })
+                    })
+                } else {
+                    $('#stale-devices-wrapper').addClass('d-none').removeClass('d-inline');
+                }
+
+                // Devices with no Helium Litres value
+                if (data.warnings.no_value_devices.length > 0) {
+                    // Display the warning icon
+                    $('#no-value-devices-wrapper').addClass('d-inline').removeClass('d-none');
+
+                    const device_count = data.warnings.no_value_devices.reduce((count, current) => count + current.length, 0);
+
+                    // Append message with number of devices
+                    $('#no-value-devices-dd-menu').append(
+                        '<li>' +
+                        device_count +
+                        ' device(s) have no He L value' +
+                        '</li>' +
+                        '<hr class="my-2">'
+                    )
+
+                    
+                    // Append list of device names
+                    data.warnings.no_value_devices.forEach(coord_devices => {
+                        coord_devices.forEach(device => {
+                            $('#no-value-devices-dd-menu').append(
+                                '<li class="font-weight-light">' +
+                                device +
+                                '</li>'
+                            )
+                        })
+                    })
+                } else {
+                    $('#no-value-devices-wrapper').addClass('d-none').removeClass('d-inline');
+                }
+
+            }
+        });
+    }
+
     setWarnings();
     setInterval( setWarnings, 300000 );
 });
--- static/js/custom/building-main-values.js
+++ static/js/custom/building-main-values.js
@@ -2,19 +2,19 @@
 
 var buildingDataURL = JSON.parse(document.getElementById('general-data-url').textContent);
 
-function updateValues() {
-    $.ajax({
-        url: buildingDataURL,
-        dataType: 'json',
-        success: function(data) {
-            $("#he-transport").text(data["he_total"]);
-            $("#oxygen-level").text(data["oxygen"]);
-            $("#purity-level").text(data["purity"]);
-        }
-    });
-}
-
 $(document).ready(function() {
+    function updateValues() {
+        $.ajax({
+            url: buildingDataURL,
+            dataType: 'json',
+            success: function(data) {
+                $("#he-transport").text(data["he_total"]);
+                $("#oxygen-level").text(data["oxygen"]);
+                $("#purity-level").text(data["purity"]);
+            }
+        });
+    }
+
     updateValues();
     setInterval( updateValues, 300000 ); // 5 min
-});
+})
