4147b254392e894b2b71e2ba6d5f782231128218
Mihai Stavila <mihai.stavila@stfc.ac.uk>
2021-04-29 18:10:05 +0100

    Update secret_key.txt path

--- project/settings.py
+++ project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('./project/secret_key.txt') as f:
+with open(os.path.join(BASE_DIR, 'project', 'secret_key.txt')) as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
